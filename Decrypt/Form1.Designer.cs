97ab2ea2c169ecfc11a71271f9621072b755500b
Shablevskiy Nikita <chitbomb@gmail.com>
2019-05-09 20:11:49 +0300

    Добавьте файлы проекта.

---
+++ Decrypt/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Decrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Exit = new System.Windows.Forms.Button();
+            this.OpenFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Instruction = new System.Windows.Forms.Button();
+            this.textBox_d = new System.Windows.Forms.TextBox();
+            this.textBox_n = new System.Windows.Forms.TextBox();
+            this.Decrypt = new System.Windows.Forms.Button();
+            this.OpenDecrypted = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Exit
+            // 
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Exit.Location = new System.Drawing.Point(500, 359);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(146, 39);
+            this.Exit.TabIndex = 0;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OpenFile.Location = new System.Drawing.Point(53, 78);
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(145, 40);
+            this.OpenFile.TabIndex = 5;
+            this.OpenFile.Text = "Open file";
+            this.OpenFile.UseVisualStyleBackColor = true;
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(49, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Choose file to decrypt information in it";
+            // 
+            // Instruction
+            // 
+            this.Instruction.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Instruction.Location = new System.Drawing.Point(500, 78);
+            this.Instruction.Name = "Instruction";
+            this.Instruction.Size = new System.Drawing.Size(146, 40);
+            this.Instruction.TabIndex = 8;
+            this.Instruction.Text = "Instruction";
+            this.Instruction.UseVisualStyleBackColor = true;
+            this.Instruction.Click += new System.EventHandler(this.Instruction_Click);
+            // 
+            // textBox_d
+            // 
+            this.textBox_d.Location = new System.Drawing.Point(53, 199);
+            this.textBox_d.Name = "textBox_d";
+            this.textBox_d.Size = new System.Drawing.Size(100, 22);
+            this.textBox_d.TabIndex = 9;
+            // 
+            // textBox_n
+            // 
+            this.textBox_n.Location = new System.Drawing.Point(241, 199);
+            this.textBox_n.Name = "textBox_n";
+            this.textBox_n.Size = new System.Drawing.Size(100, 22);
+            this.textBox_n.TabIndex = 10;
+            // 
+            // Decrypt
+            // 
+            this.Decrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Decrypt.Location = new System.Drawing.Point(53, 268);
+            this.Decrypt.Name = "Decrypt";
+            this.Decrypt.Size = new System.Drawing.Size(145, 40);
+            this.Decrypt.TabIndex = 11;
+            this.Decrypt.Text = "Decrypt";
+            this.Decrypt.UseVisualStyleBackColor = true;
+            this.Decrypt.Click += new System.EventHandler(this.Decrypt_Click);
+            // 
+            // OpenDecrypted
+            // 
+            this.OpenDecrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OpenDecrypted.Location = new System.Drawing.Point(53, 359);
+            this.OpenDecrypted.Name = "OpenDecrypted";
+            this.OpenDecrypted.Size = new System.Drawing.Size(213, 40);
+            this.OpenDecrypted.TabIndex = 12;
+            this.OpenDecrypted.Text = "Open decrypted data";
+            this.OpenDecrypted.UseVisualStyleBackColor = true;
+            this.OpenDecrypted.Click += new System.EventHandler(this.OpenDecrypted_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(211, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "n=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "d=";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(49, 150);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(157, 20);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Enter your open key";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(496, 34);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(161, 20);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "How to use program";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.OpenDecrypted);
+            this.Controls.Add(this.Decrypt);
+            this.Controls.Add(this.textBox_n);
+            this.Controls.Add(this.textBox_d);
+            this.Controls.Add(this.Instruction);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OpenFile);
+            this.Controls.Add(this.Exit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Decryption program";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button OpenFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Instruction;
+        private System.Windows.Forms.TextBox textBox_d;
+        private System.Windows.Forms.TextBox textBox_n;
+        private System.Windows.Forms.Button Decrypt;
+        private System.Windows.Forms.Button OpenDecrypted;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Decrypt/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+
+namespace Decrypt
+{
+    public partial class Form1 : Form
+    {
+        public string fileName;
+        public string saveName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "(*.txt) | *.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+            }
+        }
+
+        private void Decrypt_Click(object sender, EventArgs e)
+        {
+            if ((textBox_d.Text.Length > 0) && (textBox_n.Text.Length > 0))
+            {
+                long d;
+                long n;
+                try
+                {
+                    d = Convert.ToInt64(textBox_d.Text);
+                    n = Convert.ToInt64(textBox_n.Text);
+                   
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Invalid input format ");
+                    return;
+                }
+                //string s = "";
+                List<string> input = new List<string>();
+
+                StreamReader sr = new StreamReader(fileName, Encoding.Default);
+                while (!sr.EndOfStream)
+                {
+                    input.Add(sr.ReadLine());
+                }
+                sr.Close();
+
+                string result = RSA_Dedoce(input, d, n);
+
+
+                saveFileDialog1.Filter = "(*.txt) | *.txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    // получаем выбранный файл
+                    saveName = saveFileDialog1.FileName;
+
+                }
+
+
+                StreamWriter sw = new StreamWriter(saveName);
+                sw.WriteLine(result);
+                sw.Close();
+                MessageBox.Show("Your data was decrypted");
+            }
+            else
+                MessageBox.Show("Enter d or n!");
+        }
+        private string RSA_Dedoce(List<string> input, long d, long n)
+        {
+            string result = "";
+
+            BigInteger bi= new BigInteger();
+
+            foreach (string item in input)
+            {
+                bi = BigInteger.Pow(Convert.ToInt32(item), (int)d);
+                BigInteger degree = new BigInteger((int)n);
+                bi = bi % degree;
+                result += (char)Convert.ToInt32(bi.ToString());
+            }
+
+            return result;
+        }
+        private void OpenDecrypted_Click(object sender, EventArgs e)
+        {
+            Process.Start(saveName);
+        }
+
+        private void Instruction_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Аби розшифрувати ваші дані необіхдно :" +
+                "\n 1. Оберіть шлях до файлу(у форматі txt) з зашифрованими даними" +
+                "\n 2. Введіть відкритий ключ(d і n відповідно) " +
+                "\n 3. Натисність кнопку Decrypt" +
+                "\n 4. Аби побачити отримані дані натисніть кнопку Open decrypted file або зробіть це вручну ");
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
